d316c1d670de027675c1ec8d1a48f0945a703a80
harshal3416 <harshalbelamkar5@gmail.com>
2023-01-09 10:17:10 +0530

    browser router

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/harshal-portfolio'>
+    <BrowserRouter basename='/harshal-portfolio'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
     ,
   </React.StrictMode>,
   document.getElementById('root')
